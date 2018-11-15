50c857413a78adae2a95f6be33f050888575ef21
ch <2630601704@qq.com>
2018-11-15 18:40:31 +0800

    test

---
+++ app/src/main/java/study/ch/helloworld/chActivity.java
@@ -0,0 +1,13 @@
+package study.ch.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch);
+    }
+}
