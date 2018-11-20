3331cf835b4f6938f81af6ff7c3aceffa808aaa5
ch <2630601704@qq.com>
2018-11-20 22:10:25 +0800

    day 5

--- app/src/main/java/study/ch/helloworld/chActivity.java
+++ app/src/main/java/study/ch/helloworld/chActivity.java
@@ -1,53 +1,55 @@
 package study.ch.helloworld;
 
-import android.app.Activity;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ImageSpan;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class chActivity extends Activity {
-    private Button btnOne;
-    private Button btnTwo;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+
+public class chActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private ToggleButton tbtn_open;
+    private Switch swh_status;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_ch);
 
-//        btnOne = (Button) findViewById(R.id.submit1);
-//        btnTwo = (Button) findViewById(R.id.submit2);
-//        btnTwo.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(btnTwo.getText().toString().equals("按钮不可用")){
-//                    btnOne.setEnabled(false);
-//                    btnTwo.setText("按钮可用");
-//                }else{
-//                    btnOne.setEnabled(true);
-//                    btnTwo.setText("按钮不可用");
-//                }
-//            }
-//        });
+        tbtn_open = (ToggleButton)findViewById(R.id.tbtn_open);
+        swh_status = (Switch)findViewById(R.id.swh_status);
+        tbtn_open.setOnCheckedChangeListener(this);
+        swh_status.setOnCheckedChangeListener(this);
+    }
 
-//        btn_add = (Button) findViewById(R.id.submit);
-//        edit_one = (EditText) findViewById(R.id.edit);
-//
-//        btn_add.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SpannableString spanStr = new SpannableString("imge");
-//                Drawable drawable = chActivity.this.getResources().getDrawable(R.drawable.ptr);
-//                drawable.setBounds(0,0,drawable.getIntrinsicWidth(),drawable.getIntrinsicHeight());
-//                ImageSpan span = new ImageSpan(drawable,ImageSpan.ALIGN_BASELINE);
-//                spanStr.setSpan(span,0,4,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//                int cursor = edit_one.getSelectionStart();
-//                edit_one.getText().insert(cursor, spanStr);
-//            }
-//        });
+    /**
+     * Called when the checked state of a compound button has changed.
+     *
+     * @param compoundButton The compound button view whose state has changed.
+     * @param b  The new checked state of buttonView.
+     */
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId())
+        {
+            case R.id.tbtn_open:
+                if(compoundButton.isChecked())
+                    Toast.makeText(this, "打开声音", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "关闭声音", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.swh_status:
+                if(compoundButton.isChecked())
+                    Toast.makeText(this, "开关:ON", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "开关:OFF", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 }
 
